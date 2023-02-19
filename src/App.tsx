81ea01f0785dfc510956ee9e1ba7109ea6e52a23
sathithyayogi05 <112856273+sathithyayogi05@users.noreply.github.com>
2023-02-19 18:09:34 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Analytics from './components/Analytics'
+import { Hero } from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Hero/>
+      <Analytics/>
+    </div>
+  )
+}
+
+export default App
