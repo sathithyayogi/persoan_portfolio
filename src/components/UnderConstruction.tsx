7831549a983573e0d849d0c71edad6743b21904a
sathithyayogi05 <112856273+sathithyayogi05@users.noreply.github.com>
2023-02-26 19:42:41 +0530

    under construction tag

---
+++ src/components/UnderConstruction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+  return (
+    <div className='flex justify-center text-red-500 font-bold uppercase bg-yellow-300'>Under Construction</div>
+  )
+}
+
+export default UnderConstruction
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import BlogSubscribe from '../components/BlogSubscribe';
 import Experience from '../components/Experience';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import UnderConstruction from '../components/UnderConstruction';
 ;
 const Home = () => {
     return (
         <>
             <Navbar />
             {/* <Hero /> */}
+            <UnderConstruction/>
             <About />
             <BlogSubscribe/>
             <Experience/>
