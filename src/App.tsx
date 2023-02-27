c850a4545c80ec76851730a527a09980ff8b9005
sathithyayogi05 <112856273+sathithyayogi05@users.noreply.github.com>
2023-02-27 23:28:44 +0530

    added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Techstack from './pages/Techstack';
-;
+import ReactGA from 'react-ga';
+import { useEffect } from "react";
+
 
 function App() {
+  const TRACKING_ID = 'G-QQ2LR9T5CS';
+  ReactGA.initialize(TRACKING_ID);
+
+  useEffect(()=>{
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[])
+
 
   return (
     <div className="App">
