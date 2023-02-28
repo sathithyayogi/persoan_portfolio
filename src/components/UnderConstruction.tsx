3dad91cc388d38df6e2d6c19c74bbd8838d73a1d
sathithyayogi05 <112856273+sathithyayogi05@users.noreply.github.com>
2023-02-28 23:57:30 +0530

    note change

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UnderConstruction = () => {
   return (
-    <div className='flex justify-center text-red-500 font-bold uppercase bg-yellow-300'>Under Construction</div>
+    <div className='flex justify-center text-red-500 font-bold uppercase bg-yellow-300'>Under Construction | If you have any suggestion or Feedback you can drop that in Chat Box 🙏🏼</div>
   )
 }
 
