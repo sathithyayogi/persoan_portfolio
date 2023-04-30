8688b1b37f8a6231e0649e9c3408061ef55cb893
sathithyayogi05 <112856273+sathithyayogi05@users.noreply.github.com>
2023-04-30 21:47:11 +0530

    tech stack page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,46 @@ import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import TechStack from './pages/TechStack';
 import ReactGA from "react-ga4";
-
+import { GrowthBook } from "@growthbook/growthbook-react";
 import { useEffect } from "react";
+import { GrowthBookProvider } from "@growthbook/growthbook-react";
 
+const growthbook = new GrowthBook({
+  apiHost: "http://localhost:3100",
+  clientKey: "sdk-KYTLZ8mqyNiMY3MM",
+  enableDevMode: true,
+  trackingCallback: (experiment, result) => {
+    // TODO: Use your real analytics tracking system
+    console.log("Viewed Experiment", {
+      experimentId: experiment.key,
+      variationId: result.key
+    });
+  }
+});
+
+if (growthbook.isOn("testing")) {
+  console.log("Feature is enabled!")
+}
+
+growthbook.setAttributes({
+  "id": "foo",
+  "deviceId": "foo",
+  "company": "foo",
+  "loggedIn": true,
+  "employee": true,
+  "country": "foo",
+  "browser": "foo",
+  "url": "foo"
+});
 
 function App() {
+  
+  useEffect(() => {
+    // Load features asynchronously when the app renders
+    growthbook.loadFeatures();
+  }, []);
+
+
   useEffect(()=>{
 
     const TRACKING_ID = 'G-QQ2LR9T5CS';
@@ -16,10 +51,12 @@ function App() {
 
   return (
     <div className="App">
+      <GrowthBookProvider growthbook={growthbook}>
          <Routes>
          <Route path='/' element={<Home />} />
          <Route path='/tech-stack' element={<TechStack />} />
       </Routes>
+      </GrowthBookProvider>
     </div>
   )
 }
